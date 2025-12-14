ceb428c5926d647ebd0400ad8458004be23419a5
sirbugsalot <nadav.eldar@hotmail.com>
2025-12-14 19:02:54 +0100

    remove sliders in favor of movable dots

--- app/userProfile/src/constants.js
+++ app/userProfile/src/constants.js
@@ -28,7 +28,7 @@ export const DEFAULT_CUSTOMIZATION = {
         // and should NOT be calculated with screen dimensions here, as that causes
         // circular dependency issues and mismatched type errors.
         width: 100, // Default slider percentage for width
-        height: 100, // Default slider percentage for height
+        height: 10, // Default slider percentage for height
         waist: 10, // Default slider percentage for waist
     }
 };
@@ -36,10 +36,15 @@ export const DEFAULT_CUSTOMIZATION = {
 // SVG ViewBox container constants (screen pixel values for the <View> container)
 // User requested: 90% of frame width (screen width), 40% of frame height (screen height)
 export const WIDTH_VIEWBOX = windowWidth * 0.8;
-export const HEIGHT_VIEWBOX = windowHeight * 0.35;
+export const HEIGHT_VIEWBOX = windowHeight * 0.4;
 // The Y-anchor for the bottom of the shape, as a percentage of the VIEWBOX height (not window height)
 // This value is used by the EggPreviewSVG to map its internal 100x100 drawing space.
-export const EGG_VIEWBOX_BASE_Y = 70; // 70 units from the top of the 100-unit viewbox (i.e., 70% down)
+export const EGG_VIEWBOX_BASE_Y = HEIGHT_VIEWBOX*0.8; // 70 units from the top of the 100-unit viewbox (i.e., 70% down)
 
 // Waist constraints (percentage of total height from top)
 export const WAIST_MARGIN_PERCENTAGE = 10; // 10% margin from top/bottom
+
+export const MAX_HEIGHT = HEIGHT_VIEWBOX*0.4;
+export const MAX_WIDTH = WIDTH_VIEWBOX*0.7;
+export const MIN_WIDTH = WIDTH_VIEWBOX*0.1;
+export const MIN_HEIGHT = HEIGHT_VIEWBOX*0.05;
