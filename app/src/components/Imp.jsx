b76ffbe69c44e3f55309030865fb80ddca098abc
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-01-04 00:49:55 +0100

    Update Imp.jsx with egg

--- app/src/components/Imp.jsx
+++ app/src/components/Imp.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Svg, { Path, Circle, G } from 'react-native-svg';
-import { Oval } from '../src/components/shapes';
+import { Oval } from '../components/shapes';
 
 
 const { 
