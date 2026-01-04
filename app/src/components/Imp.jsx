6f1d333456276a8a48bfd2d34b2f9fd9ec5e93ec
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-01-04 14:43:10 +0100

    Update Imp.jsx

--- app/src/components/Imp.jsx
+++ app/src/components/Imp.jsx
@@ -32,7 +32,7 @@ const Imp = ({
                     d={`M ${pos.x} ${pos.y} 
                        Q ${pos.x - 15} ${pos.y + 5}, ${pos.x - 15} ${pos.y + 13}
                        Q ${pos.x - 10} ${pos.y + 13}, ${pos.x - 15} ${pos.y + 17}
-                       T ${pos.x - 20} ${pos.y + 17}
+                       Q ${pos.x - 17} ${pos.y + 20}, ${pos.x - 20} ${pos.y + 17}
                        L ${pos.x - shape.wx/2} ${shape.wy}
                        Z`} 
                     stroke="#FF0000" 
