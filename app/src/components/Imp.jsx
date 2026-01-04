a92f4b15ad2a5fc1a616df97db2604ddb3fa273a
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-01-05 00:07:13 +0100

    Update Imp.jsx right foot good

--- app/src/components/Imp.jsx
+++ app/src/components/Imp.jsx
@@ -44,7 +44,7 @@ const Imp = ({
                 <Path 
                     d={`M ${pos.x + 2} ${pos.y} 
                        Q ${pos.x + 8} ${pos.y + 10}, ${pos.x + 8} ${pos.y + 14}
-                       Q ${pos.x + 12} ${pos.y + 18}, ${pos.x + 17} ${pos.y + 15}
+                       Q ${pos.x + 12} ${pos.y + 17}, ${pos.x + 18} ${pos.y + 15}
                        Q ${pos.x + 19} ${pos.y + 14}, ${pos.x + 17} ${pos.y + 12}
                        Q ${pos.x + shape.wx/2 + 3} ${shape.wy + 25}, ${pos.x + shape.wx/2 - 10} ${shape.wy}
                        Z`} 
@@ -65,7 +65,7 @@ const Imp = ({
                 {/* Pivot Point for debugging */}
                 <Circle cx={pos.x + 2} cy={pos.y} r="1" fill="#FF0000" opacity="0.7" />
                 <Circle cx={pos.x + 8} cy={pos.y + 14} r="1" fill="#333" opacity="0.5" />
-                <Circle cx={pos.x + 17} cy={pos.y + 15} r="1" fill="#333" opacity="0.5" />
+                <Circle cx={pos.x + 18} cy={pos.y + 15} r="1" fill="#333" opacity="0.5" />
                 <Circle cx={pos.x + 17} cy={pos.y + 12} r="1" fill="#333" opacity="0.5" />
             </G>
         </Svg>
