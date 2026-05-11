3141bef8fadd2515566ed3388f4f6cb5c07f081b
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-05-11 20:37:55 +0200

    bug fix shapes.jsx

--- app/src/components/shapes.jsx
+++ app/src/components/shapes.jsx
@@ -65,7 +65,7 @@ export const Arms = ({ pos, shape, color, length = 1.0 }) => {
         <>
             {/* --- Left Arm --- */}
             <Path 
-                d={`M ${pos.x - wx /2} ${pos.y - way*2} 
+                d={`M ${pos.x - wx /2} ${pos.y - wy *2} 
                    Q ${pos.x - 10} ${pos.y - 20}, ${pos.x - 17} ${pos.y - 25}
                    Q ${pos.x - 10} ${pos.y - 22}, ${pos.x - 12} ${pos.y - 20}
                    Q ${pos.x - 5} ${pos.y - 25}, ${pos.x} ${pos.y - 23}
