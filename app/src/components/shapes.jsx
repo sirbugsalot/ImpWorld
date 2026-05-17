439a56f40210dd9e7c8fe02e9233a17f3d7f2197
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-05-17 12:38:53 +0200

    Update shapes.jsx

--- app/src/components/shapes.jsx
+++ app/src/components/shapes.jsx
@@ -100,11 +100,11 @@ export const LeftArm = ({ pos, shape, color, length = 1.0 }) => {
             {/* --- LEFT Arm --- */}
             <Path 
                 d={`M ${pos.x + 0.5*0.8*wx} ${pos.y - 0.4*hy} 
-                   Q ${pos.x + 0.5*1.0*wx + reach(2)} ${pos.y - 0.42*hy}, ${pos.x + 0.5*1.2*wx + reach(2)} ${pos.y - 0.55*hy}
+                   Q ${pos.x + 0.5*0.9*wx + reach(2)} ${pos.y - 0.41*hy}, ${pos.x + 0.5*1.2*wx + reach(2)} ${pos.y - 0.55*hy}
                    Q ${pos.x + 0.5*1.3*wx + reach(2)} ${pos.y - 0.6*hy}, ${pos.x + 0.5*1.4*wx + reach(2)} ${pos.y - 0.6*hy}
-                   Q ${pos.x + 0.5*1.45*wx + reach(2)} ${pos.y - 0.5*hy}, ${pos.x + 0.5*1.5*wx + reach(2)} ${pos.y - 0.47*hy}
-                   Q ${pos.x + 0.5*1.45*wx + reach(2)} ${pos.y - 0.45*hy}, ${pos.x + 0.5*1.4*wx + reach(2)} ${pos.y - 0.4*hy}
-                   Q ${pos.x + 0.5*1.1*wx + reach(2)} ${pos.y - 0.25*hy}, ${pos.x + 0.5*0.9*wx} ${pos.y - 0.25*hy}
+                   Q ${pos.x + 0.5*1.47*wx + reach(2)} ${pos.y - 0.5*hy}, ${pos.x + 0.5*1.5*wx + reach(2)} ${pos.y - 0.47*hy}
+                   Q ${pos.x + 0.5*1.45*wx + reach(2)} ${pos.y - 0.42*hy}, ${pos.x + 0.5*1.4*wx + reach(2)} ${pos.y - 0.4*hy}
+                   Q ${pos.x + 0.5*1.0*wx + reach(2)} ${pos.y - 0.25*hy}, ${pos.x + 0.5*0.9*wx} ${pos.y - 0.25*hy}
                    `}
                 stroke="#111" 
                 strokeWidth="1" 
