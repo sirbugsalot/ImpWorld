9fd4a147381dacb43715c81e119229eb559c7a4d
sirbugsalot <nadav.eldar@hotmail.com>
2026-05-17 20:08:51 +0200

    smaller pencil

--- app/src/components/Objects.jsx
+++ app/src/components/Objects.jsx
@@ -11,16 +11,16 @@ export const Pencil = ({ pos, color= "#f9c92a" }) => {
             Path logic: given pos(x,y) is the center
             Color logic: main color of the pencil*/}
             <Path 
-                d={`M ${pos.x - 0.2*pos.x} ${pos.y - pos.y - 0.4*pos.y}
-                    L ${pos.x + 0.2*pos.x} ${pos.y - pos.y + 0.4*pos.y}
-                    Q ${pos.x + 0.195*pos.x} ${pos.y + 0.35*pos.y}, ${pos.x + 0.19*pos.x} ${pos.y + 0.38*pos.y}
-                    L ${pos.x - 0.19*pos.x} ${pos.y - 0.38*pos.y}
-                    L ${pos.x - 0.22*pos.x} ${pos.y - 0.44*pos.y}
-                    L ${pos.x + 0.22*pos.x} ${pos.y + 0.44*pos.y}
-                    Q ${pos.x + 0.215*pos.x} ${pos.y + 0.42*pos.y}, ${pos.x + 0.21*pos.x} ${pos.y + 0.42*pos.y}
-                    Q ${pos.x + 0.205*pos.x} ${pos.y + 0.38*pos.y}, ${pos.x + 0.2*pos.x} ${pos.y + 0.4*pos.y}
-                    M ${pos.x + 0.21*pos.x} ${pos.y + 0.42*pos.y}
-                    L ${pos.x - 0.21*pos.x} ${pos.y - 0.42*pos.y}
+                d={`M ${pos.x - 0.02*pos.x} ${pos.y - pos.y - 0.04*pos.y}
+                    L ${pos.x + 0.02*pos.x} ${pos.y - pos.y + 0.04*pos.y}
+                    Q ${pos.x + 0.0195*pos.x} ${pos.y + 0.035*pos.y}, ${pos.x + 0.019*pos.x} ${pos.y + 0.038*pos.y}
+                    L ${pos.x - 0.019*pos.x} ${pos.y - 0.038*pos.y}
+                    L ${pos.x - 0.022*pos.x} ${pos.y - 0.044*pos.y}
+                    L ${pos.x + 0.022*pos.x} ${pos.y + 0.044*pos.y}
+                    Q ${pos.x + 0.0215*pos.x} ${pos.y + 0.042*pos.y}, ${pos.x + 0.021*pos.x} ${pos.y + 0.042*pos.y}
+                    Q ${pos.x + 0.0205*pos.x} ${pos.y + 0.038*pos.y}, ${pos.x + 0.02*pos.x} ${pos.y + 0.04*pos.y}
+                    M ${pos.x + 0.021*pos.x} ${pos.y + 0.042*pos.y}
+                    L ${pos.x - 0.021*pos.x} ${pos.y - 0.042*pos.y}
                     `} 
                 stroke="#111" 
                 strokeWidth="0.5" 
