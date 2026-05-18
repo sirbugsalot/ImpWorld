7b33d4c5385d83d600fdffea911c96f1e1740e53
sirbugsalot <nadav.eldar@hotmail.com>
2026-05-18 21:22:28 +0200

    pencil adjustments with markers

--- app/src/components/Objects.jsx
+++ app/src/components/Objects.jsx
@@ -1,4 +1,4 @@
-import { Path, Circle} from 'react-native-svg';
+import { Circle, Path } from 'react-native-svg';
 
 /**
 Position is obtained from within the hand
@@ -11,8 +11,8 @@ export const Pencil = ({ pos, color= "#f9c92a" }) => {
             Path logic: given pos(x,y) is the center
             Color logic: main color of the pencil*/}
             <Path 
-                d={`M ${pos.x - 0.02*pos.x} ${pos.y - pos.y - 0.04*pos.y}
-                    L ${pos.x + 0.02*pos.x} ${pos.y - pos.y + 0.04*pos.y}
+                d={`M ${pos.x - 0.02*pos.x} ${pos.y - 0.04*pos.y}
+                    L ${pos.x + 0.02*pos.x} ${pos.y + 0.04*pos.y}
                     Q ${pos.x + 0.0195*pos.x} ${pos.y + 0.035*pos.y}, ${pos.x + 0.019*pos.x} ${pos.y + 0.038*pos.y}
                     L ${pos.x - 0.019*pos.x} ${pos.y - 0.038*pos.y}
                     L ${pos.x - 0.022*pos.x} ${pos.y - 0.044*pos.y}
@@ -27,7 +27,15 @@ export const Pencil = ({ pos, color= "#f9c92a" }) => {
                 fill={color}
                 strokeLinecap="round" 
             />
-            <Circle cx={pos.x} cy={pos.y} r="3" fill="#333" />
+            <Circle cx={pos.x - 0.02*pos.x} cy={pos.y - 0.04*pos.y} r="1" fill="#000000" />
+            <Circle cx={pos.x + 0.02*pos.x} cy={pos.y + 0.04*pos.y} r="1" fill="#434141" />
+            <Circle cx={pos.x + 0.019*pos.x} cy={pos.y + 0.038*pos.y} r="1" fill="#6c6a6a" />
+            <Circle cx={pos.x - 0.019*pos.x} cy={pos.y - 0.038*pos.y} r="1" fill="#9b9999" />
+            <Circle cx={pos.x - 0.022*pos.x} cy={pos.y - 0.044*pos.y} r="1" fill="#f4bbbb" />
+            <Circle cx={pos.x + 0.022*pos.x} cy={pos.y + 0.044*pos.y} r="1" fill="#f79e9e" />
+            <Circle cx={pos.x + 0.021*pos.x} cy={pos.y + 0.042*pos.y} r="1" fill="#f79e9e" />
+            <Circle cx={pos.x + 0.02*pos.x} cy={pos.y + 0.04*pos.y} r="1" fill="#f87070" />
+            <Circle cx={pos.x - 0.021*pos.x} cy={pos.y - 0.042*pos.y} r="1" fill="#f92525" />
         </>
     );
 };
