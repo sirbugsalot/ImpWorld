6a1a75163a3755714b66aa00534e01638bc8cc1b
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-05-19 08:27:00 +0200

    Update Objects.jsx try

--- app/src/components/Objects.jsx
+++ app/src/components/Objects.jsx
@@ -4,19 +4,17 @@ import { Circle, Path } from 'react-native-svg';
 Position is obtained from within the hand
 */
 export const Pencil = ({ pos, color= "#f9c92a" }) => {
-    const shift_x = -0.1;
-    const multiplier_x = 0.5;
-    const multiplier_y = 0.5;
+    
     return (
         <>
             {/* --- Pencil Body--- 
             Path logic: given pos(x,y) is the center
             Color logic: main color of the pencil*/}
             <Path 
-                d={`M ${pos.x - 0.10*pos.x} ${pos.y + 0.20*pos.y}
-                    L ${pos.x + 0.10*pos.x} ${pos.y - 0.20*pos.y}
-                    Q ${pos.x + 0.075*pos.x} ${pos.y - 0.195*pos.y}, ${pos.x + 0.05*pos.x } ${pos.y - 0.19*pos.y}
-                    L ${pos.x - 0.05*pos.x } ${pos.y + 0.19*pos.y}
+                d={`M ${pos.x - 0.05*pos.x } ${pos.y - 0.19*pos.y}
+                    L ${pos.x + 0.05*pos.x } ${pos.y + 0.19*pos.y}
+                    Q ${pos.x - 0.03125*pos.x} ${pos.y + 0.185*pos.y}, ${pos.x - 0.025*pos.x } ${pos.y + 0.18*pos.y}
+                    L ${pos.x - 0.075*pos.x } ${pos.y - 0.195*pos.y}
                     Z`} 
                 stroke="#111" 
                 strokeWidth="0.5" 
