f5b736e552afd8a766b70a3da815a3ca1a93a0ae
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-05-19 08:10:26 +0200

    Update Objects.jsx simplified pencil

--- app/src/components/Objects.jsx
+++ app/src/components/Objects.jsx
@@ -13,31 +13,16 @@ export const Pencil = ({ pos, color= "#f9c92a" }) => {
             Path logic: given pos(x,y) is the center
             Color logic: main color of the pencil*/}
             <Path 
-                d={`M ${pos.x - 0.10*pos.x} ${pos.y - 0.4*pos.y*multiplier_y}
-                    L ${pos.x + 0.10*pos.x} ${pos.y + 0.4*pos.y*multiplier_y}
-                    Q ${pos.x + 0.075*pos.x} ${pos.y + 0.35*pos.y*multiplier_y}, ${pos.x + 0.05*pos.x } ${pos.y + 0.38*pos.y*multiplier_y}
-                    L ${pos.x - 0.075*pos.x } ${pos.y - 0.38*pos.y*multiplier_y}
-                    L ${pos.x - 0.20 *pos.x } ${pos.y - 0.44*pos.y*multiplier_y}
-                    L ${pos.x + 0.20 *pos.x } ${pos.y + 0.44*pos.y*multiplier_y}
-                    Q ${pos.x + 0.175 *pos.x } ${pos.y + 0.42*pos.y*multiplier_y}, ${pos.x + 0.15 *pos.x } ${pos.y + 0.42*pos.y*multiplier_y}
-                    Q ${pos.x + 0.125 *pos.x } ${pos.y + 0.38*pos.y*multiplier_y}, ${pos.x + 0.10 *pos.x } ${pos.y + 0.4*pos.y*multiplier_y}
-                    M ${pos.x + 0.15 *pos.x } ${pos.y + 0.42*pos.y*multiplier_y}
-                    L ${pos.x - 0.15 *pos.x } ${pos.y - 0.42*pos.y*multiplier_y}
-                    `} 
+                d={`M ${pos.x - 0.10*pos.x} ${pos.y - 0.20*pos.y}
+                    L ${pos.x + 0.10*pos.x} ${pos.y + 0.20*pos.y}
+                    Q ${pos.x - 0.075*pos.x} ${pos.y + 0.175*pos.y}, ${pos.x + 0.05*pos.x } ${pos.y + 0.19*pos.y}
+                    L ${pos.x - 0.05*pos.x } ${pos.y - 0.19*pos.y}
+                    Z`} 
                 stroke="#111" 
                 strokeWidth="0.5" 
                 fill={color}
                 strokeLinecap="round" 
             />
-            <Circle cx={pos.x - 0.20 *pos.x } cy={pos.y - 0.40*pos.y*multiplier_y} r="1" fill="#000000" />
-            <Circle cx={pos.x + 0.20 *pos.x } cy={pos.y + 0.40*pos.y*multiplier_y} r="1" fill="#434141" />
-            <Circle cx={pos.x + 0.19 *pos.x } cy={pos.y + 0.38*pos.y*multiplier_y} r="1" fill="#6c6a6a" />
-            <Circle cx={pos.x - 0.19 *pos.x } cy={pos.y - 0.38*pos.y*multiplier_y} r="1" fill="#9b9999" />
-            <Circle cx={pos.x - 0.22 *pos.x } cy={pos.y - 0.44*pos.y*multiplier_y} r="1" fill="#f4bbbb" />
-            <Circle cx={pos.x + 0.20 *pos.x } cy={pos.y + 0.44*pos.y*multiplier_y} r="1" fill="#f79e9e" />
-            <Circle cx={pos.x + 0.21 *pos.x } cy={pos.y + 0.42*pos.y*multiplier_y} r="1" fill="#f79e9e" />
-            <Circle cx={pos.x + 0.20 *pos.x } cy={pos.y + 0.40*pos.y*multiplier_y} r="1" fill="#f87070" />
-            <Circle cx={pos.x - 0.21 *pos.x } cy={pos.y - 0.42*pos.y*multiplier_y} r="1" fill="#f92525" />
         </>
     );
 };
