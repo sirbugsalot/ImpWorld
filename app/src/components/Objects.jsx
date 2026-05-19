468a440b12f52241700c04cc34dabd7fa99fb8ae
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-05-19 08:56:13 +0200

    Update Objects.jsx

--- app/src/components/Objects.jsx
+++ app/src/components/Objects.jsx
@@ -13,8 +13,8 @@ export const Pencil = ({ pos, color= "#f9c92a" }) => {
             <Path 
                 d={`M ${pos.x - 5} ${pos.y - 16}
                     L ${pos.x + 5} ${pos.y + 16}
-                    Q ${pos.x + 4.5} ${pos.y + 16.5}, ${pos.x + 4} ${pos.y + 17}
-                    L ${pos.x - 6} ${pos.y - 17}
+                    Q ${pos.x + 4.25} ${pos.y + 14}, ${pos.x + 4} ${pos.y + 16.5}
+                    L ${pos.x - 6} ${pos.y - 16}
                     Z`} 
                 stroke="#111" 
                 strokeWidth="0.25" 
