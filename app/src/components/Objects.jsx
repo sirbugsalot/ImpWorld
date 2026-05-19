e4a7c4813224d2b27577d198dc5b46146b374ca8
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-05-19 08:15:42 +0200

    Update Objects.jsx

--- app/src/components/Objects.jsx
+++ app/src/components/Objects.jsx
@@ -13,10 +13,10 @@ export const Pencil = ({ pos, color= "#f9c92a" }) => {
             Path logic: given pos(x,y) is the center
             Color logic: main color of the pencil*/}
             <Path 
-                d={`M ${pos.x - 0.10*pos.x} ${pos.y - 0.20*pos.y}
-                    L ${pos.x + 0.10*pos.x} ${pos.y + 0.20*pos.y}
-                    Q ${pos.x + 0.075*pos.x} ${pos.y + 0.195*pos.y}, ${pos.x + 0.05*pos.x } ${pos.y + 0.19*pos.y}
-                    L ${pos.x - 0.05*pos.x } ${pos.y - 0.19*pos.y}
+                d={`M ${pos.x - 0.10*pos.x} ${pos.y + 0.20*pos.y}
+                    L ${pos.x + 0.10*pos.x} ${pos.y - 0.20*pos.y}
+                    Q ${pos.x + 0.075*pos.x} ${pos.y - 0.195*pos.y}, ${pos.x + 0.05*pos.x } ${pos.y - 0.19*pos.y}
+                    L ${pos.x - 0.05*pos.x } ${pos.y + 0.19*pos.y}
                     Z`} 
                 stroke="#111" 
                 strokeWidth="0.5" 
