516ee4bc48e1f583b76b4338dcaba75a7de9a899
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-05-20 17:15:34 +0200

    Update shapes.jsx

--- app/src/components/shapes.jsx
+++ app/src/components/shapes.jsx
@@ -200,9 +200,9 @@ export const LeftArm = ({ pos, shape, color, length = 1.0 }) => {
                 strokeLinecap="round" 
             />
             <Path 
-                d={`M ${handCenter.x - 7.5} ${handCenter.y + 0.5}
-                    A ${5.9/2} ${4.79/2} 0 0 0 ${handCenter.x - 2.2} ${handCenter.y - 4.5} 
-                    A ${5.9/2} ${4.79/2} 0 0 0 ${handCenter.x - 7.5} ${handCenter.y + 0.5}
+                d={`M ${handCenter.x - 5.0} ${handCenter.y + 2.0}
+                    A ${6.0/2} ${4.79/2} 0 0 0 ${handCenter.x - 2.2} ${handCenter.y - 4.5} 
+                    A ${6.0/2} ${4.79/2} 0 0 0 ${handCenter.x - 5.0} ${handCenter.y + 2.0}
                    `}
                 stroke="#111" 
                 strokeWidth="1" 
