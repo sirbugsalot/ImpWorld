324a7c5bb3e7843644c07c007618baceed0e6589
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-05-22 12:19:58 +0200

    Update Circle components with new positions and sizes

--- app/src/components/shapes.jsx
+++ app/src/components/shapes.jsx
@@ -224,7 +224,8 @@ export const Ears = ({shape, color, length = 1.0 }) => {
                 fill={color}
                 strokeLinecap="round" 
             />
-            <Circle cx={center_x} cy={ref_y} r='15' fill="#333" />
+            <Circle cx={0} cy={0} r='5' fill="#333" />
+            <Circle cx={0} cy={hy + wy} r='5' fill="#111" />
         </>
     );
 };
