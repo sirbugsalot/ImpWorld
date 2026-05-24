f16714e924f37bce6f4cf3bfff060702a5174c68
sirbugsalot <nadav.eldar@hotmail.com>
2026-05-24 12:46:59 +0200

    center eyes

--- app/src/components/shapes.jsx
+++ app/src/components/shapes.jsx
@@ -214,7 +214,7 @@ export const Ears = ({pos, shape, color, length = 1.0 }) => {
             <Path 
                 d={`M ${ref_x + 2.5*scale} ${ref_y + 0.5*scale} 
                    Q ${ref_x + 5.0*scale} ${ref_y - 2.5*scale - reach(5)}, ${ref_x + 6.0*scale} ${ref_y - 3.0*scale - reach(5)}
-                   Q ${ref_x + 6.4*scale} ${ref_y - 2.8*scale - reach(5)}, ${ref_x + 6.5*scale} ${ref_y + 5.7*scale}
+                   Q ${ref_x + 6.4*scale} ${ref_y - 2.8*scale - reach(5)}, ${ref_x + 6.5*scale} ${ref_y + 5.2*scale}
                    Q ${ref_x + 4.5*scale} ${ref_y + 2.0*scale}, ${ref_x + 2.5*scale} ${ref_y + 0.5*scale}
                    M ${ref_x - 7.0*scale} ${ref_y + 6.0*scale} 
                    Q ${ref_x - 9.3*scale} ${ref_y + 1.0*scale - reach(5)}, ${ref_x - 9.0*scale} ${ref_y - 2.0*scale - reach(5)}
@@ -257,12 +257,12 @@ export const Eyes = ({pos, shape, color, length = 1.0 }) => {
     const R1    = 5.0/2*length; // Outer eye major radius
         // LH
     const r1_L  = 4.0/2*length*0.9; // Outer eye minor radius LH
-    const p1_L  = {x: ref_x - 6.0*scale*length*0.85, y: ref_y + 6.5*scale}; // Pupil radius LH
-    const p2_L  = {x: ref_x - 2.2*scale*length*0.85, y: ref_y + 6.5*scale}; // Pupil radius LH
+    const p1_L  = {x: (ref_x - 6.0*scale)*(1 - length*0.05), y: ref_y + 6.5*scale}; // Pupil radius LH
+    const p2_L  = {x: (ref_x - 2.2*scale)*(1 + length*0.05), y: ref_y + 6.5*scale}; // Pupil radius LH
         // RH
     const r1_R  = 3.0/2*length*0.85; // Outer eye minor radius RH
-    const p1_R  = {x: ref_x + 4.0*scale*length*0.80, y: ref_y + 6.5*scale}; // Pupil radius RH
-    const p2_R  = {x: ref_x + 7.0*scale*length*0.80, y: ref_y + 6.5*scale}; // Pupil radius RH    
+    const p1_R  = {x: (ref_x + 4.0*scale)*(1 - length*0.02), y: ref_y + 6.5*scale}; // Pupil radius RH
+    const p2_R  = {x: (ref_x + 7.0*scale)*(1 + length*0.02), y: ref_y + 6.5*scale}; // Pupil radius RH    
 
     return (
         <>
