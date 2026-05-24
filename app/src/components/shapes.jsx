838fb0ff00186b3973d4b624356b65f677b8311b
sirbugsalot <nadav.eldar@hotmail.com>
2026-05-24 10:51:50 +0200

    better ears

--- app/src/components/shapes.jsx
+++ app/src/components/shapes.jsx
@@ -214,15 +214,15 @@ export const Ears = ({pos, shape, color, length = 1.0 }) => {
             <Path 
                 d={`M ${ref_x + 2.5*scale} ${ref_y + 0.5*scale} 
                    Q ${ref_x + 5.0*scale} ${ref_y - 2.5*scale - reach(5)}, ${ref_x + 6.0*scale} ${ref_y - 3.0*scale - reach(5)}
-                   Q ${ref_x + 6.4*scale} ${ref_y - 2.8*scale - reach(5)}, ${ref_x + 6.3*scale} ${ref_y + 6.0*scale}
-                   Q ${ref_x - 4.0*scale} ${ref_y + 2.0*scale}, ${ref_x + 2.5*scale} ${ref_y + 0.5*scale}
+                   Q ${ref_x + 6.4*scale} ${ref_y - 2.8*scale - reach(5)}, ${ref_x + 6.5*scale} ${ref_y + 6.0*scale}
+                   Q ${ref_x + 4.0*scale} ${ref_y + 2.0*scale}, ${ref_x + 2.5*scale} ${ref_y + 0.5*scale}
                    M ${ref_x - 7.0*scale} ${ref_y + 6.0*scale} 
                    Q ${ref_x - 9.3*scale} ${ref_y + 1.0*scale - reach(5)}, ${ref_x - 9.0*scale} ${ref_y - 2.0*scale - reach(5)}
                    Q ${ref_x - 7.0*scale} ${ref_y - 1.5*scale - reach(5)}, ${ref_x - 2.5*scale} ${ref_y + 2.5*scale}
-                   Q ${ref_x - 6.0*scale} ${ref_y + 1.5*scale}, ${ref_x - 7.0*scale} ${ref_y + 6.0*scale} 
+                   Q ${ref_x - 5.5*scale} ${ref_y + 1.5*scale}, ${ref_x - 7.0*scale} ${ref_y + 4.5*scale} 
                    `} 
                 stroke="#657c7c" 
-                strokeWidth="1" 
+                strokeWidth="0.1" 
                 fill="#2d2a32"
                 strokeLinecap="round" 
             />
@@ -230,12 +230,12 @@ export const Ears = ({pos, shape, color, length = 1.0 }) => {
             <Path 
                 d={`M ${ref_x + 4.3*scale} ${ref_y + 1.5*scale} 
                    Q ${ref_x + 5.5*scale} ${ref_y - 1.8*scale - reach(5)}, ${ref_x + 6.0*scale} ${ref_y - 2.5*scale - reach(5)}
-                   Q ${ref_x + 6.1*scale} ${ref_y - 1.0*scale - reach(5)}, ${ref_x + 6.0*scale} ${ref_y + 2.5*scale}
+                   Q ${ref_x + 6.1*scale} ${ref_y - 1.0*scale - reach(5)}, ${ref_x + 6.0*scale} ${ref_y + 3.0*scale}
                    M ${ref_x - 6.8*scale} ${ref_y + 5.0*scale} 
                    Q ${ref_x - 8.9*scale} ${ref_y + 0.5*scale - reach(5)}, ${ref_x - 8.6*scale} ${ref_y - 1.5*scale - reach(5)}
                    Q ${ref_x - 7.5*scale} ${ref_y - 0.8*scale - reach(5)}, ${ref_x - 5.7*scale} ${ref_y + 3.0*scale}
                    `}
-                stroke="#111" 
+                stroke="#d59b95" 
                 strokeWidth="1" 
                 fill="#eac3b5"
                 strokeLinecap="round" 
@@ -257,27 +257,12 @@ export const Eyes = ({pos, shape, color, length = 1.0 }) => {
         <>
             {/* --- Outer Eyes --- */}
             <Path 
-                d={`M ${handCenter.x - 5.0} ${handCenter.y + 0.2}
-                    A ${5.8/2} ${4.9/2} 0 0 0 ${handCenter.x - 2.2} ${handCenter.y - 5.0} 
-                    A ${5.8/2} ${4.9/2} 0 0 0 ${handCenter.x - 5.0} ${handCenter.y + 0.2}
+                d={`M ${ref_x - 6.0*scale} ${ref_y + 6.5*scale}
+                    A ${5.0/2} ${4.0/2} 0 0 0 ${ref_x - 2.2*scale} ${ref_y - 6.4*scale}
                    `}
                 stroke="#f4e6cc" 
                 strokeWidth="0.1" 
                 fill="#f4e6cc"
-                strokeLinecap="round" 
-            />
-            {/* --- Inner Ears --- */}
-            <Path 
-                d={`M ${ref_x + 4.3} ${ref_y + 1.5} 
-                   Q ${ref_x + 5.5*scale} ${ref_y - 2.0*scale - reach(5)}, ${ref_x + 6.0*scale} ${ref_y - 2.5*scale - reach(5)}
-                   Q ${ref_x + 6.1*scale} ${ref_y - 1.0*scale - reach(5)}, ${ref_x + 6.0*scale} ${ref_y + 2.5*scale - reach(5)}
-                   M ${ref_x - 7.8*scale} ${ref_y + 4.0*scale} 
-                   Q ${ref_x - 8.9*scale} ${ref_y + 0.5*scale - reach(5)}, ${ref_x - 8.6*scale} ${ref_y - 1.5*scale - reach(5)}
-                   Q ${ref_x - 7.8*scale} ${ref_y - 0.8*scale - reach(5)}, ${ref_x - 6.0*scale} ${ref_y + 3.0*scale}
-                   `}
-                stroke="#111" 
-                strokeWidth="0.25" 
-                fill="#eac3b5"
                 strokeLinecap="round" 
             />
         </>
