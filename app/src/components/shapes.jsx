3a157263619e13cb5ad68baed6d79ee0147b0e13
sirbugsalot <118769570+sirbugsalot@users.noreply.github.com>
2026-06-28 19:09:45 +0200

    Update shapes.jsx

--- app/src/components/shapes.jsx
+++ app/src/components/shapes.jsx
@@ -304,8 +304,8 @@ export const Mouth = ({pos, shape, color, length = 1.0 }) => {
     // Calculate the outline of the mouth
     const leftCorner = { x: ref_x - (15/2) - reach(2), y: ref_y + 30 };
     const rightCorner = { x: ref_x + (15/2) + reach(2), y: ref_y + 30 };
-    const upperLip_R = -6.0;
-    const lowerLip_R = -12.0;
+    const upperLip_R = 6.0;
+    const lowerLip_R = 12.0;
     const mouthWidth = rightCorner.x - leftCorner.x; // Total width of the mouth
 
     // Teeth
@@ -338,7 +338,7 @@ export const Mouth = ({pos, shape, color, length = 1.0 }) => {
             <Path 
                 d={`M ${leftCorner.x} ${leftCorner.y}
                     A ${mouthWidth} ${upperLip_R} 0 0 0 ${rightCorner.x} ${rightCorner.y} 
-                    A ${mouthWidth} ${upperLip_R} 0 0 0 ${leftCorner.x} ${leftCorner.y} 
+                    A ${mouthWidth} ${lowerLip_R} 0 0 0 ${leftCorner.x} ${leftCorner.y} 
                    `} 
                 stroke="#6b4646" 
                 strokeWidth="0.01" 
@@ -351,7 +351,7 @@ export const Mouth = ({pos, shape, color, length = 1.0 }) => {
                     L ${teethLeftCorner.x} ${ref_y + 33.6}
                     A ${teethWidth} ${upperLip_R} 0 0 0 ${teethRightCorner.x} ${ref_y + 33.6}
                     L ${teethRightCorner.x} ${teethRightCorner.y}
-                    A ${teethWidth} ${upperLip_R} 0 0 0 ${teethLeftCorner.x} ${teethLeftCorner.y} 
+                    A ${teethWidth} ${lowerLip_R} 0 0 0 ${teethLeftCorner.x} ${teethLeftCorner.y} 
                    `} 
                 stroke="#575757" 
                 strokeWidth="0.01" 
